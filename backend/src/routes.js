3f267bee55c3b7cc491db32400e9546f244f7f79
Charles Pereira <charlesti.ptu@gmail.com>
2020-01-29 23:43:07 -0300

    UserController e gerando hash da senha no UserModel

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'teste',
-    email: 'teste@teste.com',
-    password_hash: '123123123',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
