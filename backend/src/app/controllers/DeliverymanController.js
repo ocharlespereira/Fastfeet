bd35af4ba66ad90609e4c871c310ecc57514cc24
Charles Pereira <charlesti.ptu@gmail.com>
2020-02-02 00:55:13 -0300

    Crud de Controller de deliveryman completo

--- backend/src/app/controllers/DeliverymanController.js
+++ backend/src/app/controllers/DeliverymanController.js
@@ -1,9 +1,24 @@
 import * as Yup from 'yup';
 
 import Deliveryman from '../models/Deliveryman';
-// import File from '../models/File';
+import File from '../models/File';
 
 class DeliverymanController {
+  async index(req, res) {
+    const delivery = await Deliveryman.findAll({
+      attributes: ['id', 'name', 'email', 'avatar_id'],
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
+
+    return res.json(delivery);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       name: Yup.string().required(),
@@ -26,6 +41,40 @@ class DeliverymanController {
       email,
       avatar_id,
     });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const { id } = req.params;
+
+    const develiveryId = await Deliveryman.findByPk(id);
+
+    const { name, email, avatar_id } = await develiveryId.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      avatar_id,
+    });
+  }
+
+  async delete(req, res) {
+    const develivery = await Deliveryman.findByPk(req.params.id, {
+      attributes: ['id', 'name', 'email'],
+    });
+
+    await develivery.destroy();
+
+    return res.json(develivery);
   }
 }
 
