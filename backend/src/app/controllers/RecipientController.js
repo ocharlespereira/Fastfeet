1f8cb0c265dabda557e07ec19543c0c0a75a6be7
Charles Pereira <charlesti.ptu@gmail.com>
2020-02-02 15:29:01 -0300

    RecipientController - Crud completo com validacoes

--- backend/src/app/controllers/RecipientController.js
+++ backend/src/app/controllers/RecipientController.js
@@ -1,10 +1,139 @@
+import * as Yup from 'yup';
+
 import Recipient from '../models/Recipient';
 
 class RecipientController {
-  async store(req, res) {
-    const { id } = req.body;
+  async index(req, res) {
+    const recipient = await Recipient.findAll({
+      attributes: [
+        'id',
+        'name',
+        'street',
+        'number',
+        'complement',
+        'city',
+        'state',
+        'cep',
+      ],
+    });
 
-    const recipient = await Recipient.findAll(id);
+    return res.json(recipient);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string(),
+      city: Yup.string().required(),
+      state: Yup.string().required(),
+      cep: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const { name } = req.body;
+
+    const recipientExists = await Recipient.findOne({
+      where: { name },
+      order: { name },
+    });
+
+    if (recipientExists) {
+      return res.status(400).json({ error: 'Recipient already exists.' });
+    }
+
+    const {
+      id,
+      street,
+      number,
+      complement,
+      city,
+      state,
+      cep,
+    } = await Recipient.create(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      city,
+      state,
+      cep,
+    });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      street: Yup.string(),
+      number: Yup.number(),
+      complement: Yup.string(),
+      city: Yup.string(),
+      state: Yup.string(),
+      cep: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const { id } = req.params;
+
+    const recipientId = await Recipient.findByPk(id);
+
+    const {
+      name,
+      street,
+      number,
+      complement,
+      city,
+      state,
+      cep,
+    } = await recipientId.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      complement,
+      city,
+      state,
+      cep,
+    });
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const recipientExist = await Recipient.findByPk(id);
+
+    if (!recipientExist) {
+      return res
+        .status(401)
+        .json({ worning: 'Recipient not exists for delete.' });
+    }
+
+    const recipient = await Recipient.findByPk(id, {
+      attributes: [
+        'id',
+        'name',
+        'street',
+        'number',
+        'complement',
+        'city',
+        'state',
+        'cep',
+      ],
+    });
+
+    await recipient.destroy();
 
     return res.json(recipient);
   }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,9 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
-import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+import RecipientController from './app/controllers/RecipientController';
 import FileController from './app/controllers/FileController';
 import DeliverymanController from './app/controllers/DeliverymanController';
 
@@ -18,6 +19,11 @@ routes.use(authMiddleware);
 
 routes.post('/users', UserController.store);
 routes.put('/users', UserController.update);
+
+routes.get('/recipients', RecipientController.index);
+routes.post('/recipients', RecipientController.store);
+routes.put('/recipients/:id', RecipientController.update);
+routes.delete('/recipients/:id', RecipientController.delete);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
