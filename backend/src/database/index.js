d317bfad93af7c41962f65b711986c1c031b03ff
Charles Pereira <charlesti.ptu@gmail.com>
2020-02-02 23:53:41 -0300

    Order e Signature

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -4,10 +4,12 @@ import User from '../app/models/User';
 import Recipient from '../app/models/Recipient';
 import File from '../app/models/File';
 import Deliveryman from '../app/models/Deliveryman';
+import Order from '../app/models/Order';
+import Signature from '../app/models/Signature';
 
 import databaseConfig from '../config/database';
 
-const models = [User, Recipient, File, Deliveryman];
+const models = [User, Recipient, File, Deliveryman, Order, Signature];
 
 class Database {
   constructor() {
