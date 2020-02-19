ab4f69d76d0d403591ed87cf386a069f6b7a65e5
Charles Pereira <charlesti.ptu@gmail.com>
2020-02-18 22:44:01 -0300

    Configurando variaveis de ambiente

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,14 +1,15 @@
+require('dotenv/config');
+
 module.exports = {
   dialect: 'postgres',
-  host: '192.168.99.100', // 'localhost',
+  host: process.env.DB_HOST,
   port: 5432,
-  username: 'postgres',
-  password: 'docker',
-  database: 'fastfeet',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   define: {
     timestamps: true,
     underscored: true,
     underscoredAll: true,
   },
-  timezone: '-03:00',
 };
--- backend/src/config/sentry.js
+++ backend/src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://5d1a10abb3264164847808dad2c8d339@sentry.io/2651847',
+  dsn: process.env.SENTRY_DSN,
 };
