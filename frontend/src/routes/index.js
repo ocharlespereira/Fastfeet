45c5733fdef9f005f641cb44e684ffa185978ec5
Charles Pereira <charlesti.ptu@gmail.com>
2020-02-22 17:19:45 -0300

    Criando rotas privadas

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 import Dashboard from '~/pages/Dashboard';
-import Profile from '~/pages/Profile';
+import Deliveryman from '~/pages/Deliveryman';
+import Order from '~/pages/Order';
+import Problem from '~/pages/Problem';
+import Recipient from '~/pages/Recipient';
 import SignIn from '~/pages/SignIn';
+import Route from '~/routes/Route';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/order" component={Order} isPrivate />
+      <Route path="/deliveryman" component={Deliveryman} isPrivate />
+      <Route path="/recipient" component={Recipient} isPrivate />
+      <Route path="/problem" component={Problem} isPrivate />
     </Switch>
   );
 }
