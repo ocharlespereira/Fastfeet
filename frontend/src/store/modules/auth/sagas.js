77673c260204c0e8090dcda329fd8d085ef87e82
Charles Pereira <charlesti.ptu@gmail.com>
2020-02-23 17:26:25 -0300

    Loading de autenticação - Entrar no sistema

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -3,22 +3,26 @@ import { takeLatest, call, all, put } from 'redux-saga/effects';
 import api from '~/services/api';
 import history from '~/services/history';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  api.defaults.headers.Authorization = `Bearer ${token}`;
+    api.defaults.headers.Authorization = `Bearer ${token}`;
 
-  yield put(signInSuccess(token, user));
-  history.push('/dashboard');
+    yield put(signInSuccess(token, user));
+    history.push('/dashboard');
+  } catch (error) {
+    yield put(signFailure());
+  }
 }
 
 export function signOut() {
