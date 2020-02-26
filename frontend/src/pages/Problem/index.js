cbe1c244da9cce0c1eb36c014e470ec7f378649e
Charles Pereira <charlesti.ptu@gmail.com>
2020-02-25 23:57:13 -0300

    Estilização css do Problem-Help

--- frontend/src/pages/Problem/index.js
+++ frontend/src/pages/Problem/index.js
@@ -1,7 +1,55 @@
 import React from 'react';
+import { MdAdd, MdSearch } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import ActionProblem from '~/components/Actions/Problem';
+
+import {
+  Container,
+  OrderControls,
+  Button,
+  SearchInput,
+  Content,
+  OrderTable,
+} from './styles';
 
 export default function Problem() {
-  return <h1>Problems</h1>;
+  return (
+    <Container>
+      <h1>Gerenciando destinatários</h1>
+
+      <OrderControls>
+        <SearchInput>
+          <MdSearch size={18} color="#999" />
+          <input name="search" placeholder="Buscar por destinatários" />
+        </SearchInput>
+        <Button as={Link} to="/orders/new">
+          <MdAdd size={20} color="#FFF" />
+          Cadastrar
+        </Button>
+      </OrderControls>
+
+      <Content>
+        <OrderTable>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Problema</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>#01</td>
+              <td>Infelizmente sofri um acidente que danificou a encomenda</td>
+              <td>
+                <ActionProblem />
+              </td>
+            </tr>
+          </tbody>
+        </OrderTable>
+      </Content>
+    </Container>
+  );
 }
