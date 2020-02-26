9907c09bc96b632c150d66d75c569f1f1f90c3eb
Charles Pereira <charlesti.ptu@gmail.com>
2020-02-25 22:08:19 -0300

    Estilização css de Deliverymans

--- frontend/src/pages/Deliveryman/index.js
+++ frontend/src/pages/Deliveryman/index.js
@@ -1,7 +1,63 @@
 import React from 'react';
+import { MdAdd, MdSearch } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import ActionDeliveryman from '~/components/ActionDeliveryman';
+
+import {
+  Container,
+  OrderControls,
+  Button,
+  SearchInput,
+  Content,
+  OrderTable,
+} from './styles';
 
 export default function Deliveryman() {
-  return <h1>Deliveryman</h1>;
+  return (
+    <Container>
+      <h1>Gerenciando entregadores</h1>
+
+      <OrderControls>
+        <SearchInput>
+          <MdSearch size={18} color="#999" />
+          <input name="search" placeholder="Buscar por entregadores" />
+        </SearchInput>
+        <Button as={Link} to="/orders/new">
+          <MdAdd size={20} color="#FFF" />
+          Cadastrar
+        </Button>
+      </OrderControls>
+
+      <Content>
+        <OrderTable>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Foto</th>
+              <th>Nome</th>
+              <th>Email</th>
+              <th>Ações</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>#01</td>
+              <td>
+                <span>CH</span>
+              </td>
+              <td>
+                <strong>Charles Pereira</strong>
+              </td>
+              <td>charles@teste.com</td>
+              <td>
+                <ActionDeliveryman />
+              </td>
+            </tr>
+          </tbody>
+        </OrderTable>
+      </Content>
+    </Container>
+  );
 }
