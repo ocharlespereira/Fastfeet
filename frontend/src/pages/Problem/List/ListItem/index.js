7cf1366ec43f0b469a57ce03a6e8c64f695a00d3
Charles Pereira <charlesti.ptu@gmail.com>
2020-03-09 00:14:07 -0300

    page: problem - correção conexão api II

--- frontend/src/pages/Problem/List/ListItem/index.js
+++ frontend/src/pages/Problem/List/ListItem/index.js
@@ -13,6 +13,7 @@ import { Container, ActionContainer, ModalContainer } from './styles';
 
 export default function ProblemItem({ data, updateProblems }) {
   async function handleCancel() {
+    // eslint-disable-next-line no-alert
     const confirm = window.confirm(
       'Você tem certeza que deseja cancelar a encomenda?'
     );
@@ -33,33 +34,34 @@ export default function ProblemItem({ data, updateProblems }) {
 
   return (
     <Container>
-      <small>{data._id}</small>
+      <small>#{data.delivery_id}</small>
       <small>{data.description}</small>
-      <ActionPopUp contentStyle={{ width: '200px', borderRadius: '4px' }} />
+      <ActionPopUp contentStyle={{ width: '200px', borderRadius: '4px' }}>
+        <ActionContainer>
+          <div>
+            <Modal>
+              <ModalContainer>
+                <strong>VISUALIZAR PROBLEMA</strong>
+                <p>{data.description}</p>
+              </ModalContainer>
+            </Modal>
+          </div>
 
-      <ActionContainer>
-        <div>
-          <Modal>
-            <ModalContainer>
-              <strong>VISUALIZAR PROBLEMA</strong>
-              <p>{data.description}</p>
-            </ModalContainer>
-          </Modal>
-        </div>
-
-        <div>
-          <button onClick={handleCancel} type="button">
-            <MdDeleteForever color={colors.danger} size={15} />
-            <span>Cancelar encomenda</span>
-          </button>
-        </div>
-      </ActionContainer>
+          <div>
+            <button onClick={handleCancel} type="button">
+              <MdDeleteForever color={colors.danger} size={15} />
+              <span>Cancelar encomenda</span>
+            </button>
+          </div>
+        </ActionContainer>
+      </ActionPopUp>
     </Container>
   );
 }
 
 ProblemItem.propTypes = {
   data: PropTypes.shape({
+    delivery_id: PropTypes.string.isRequired,
     _id: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
   }).isRequired,
--- frontend/src/pages/Problem/List/ListItem/styles.js
+++ frontend/src/pages/Problem/List/ListItem/styles.js
@@ -2,6 +2,87 @@ import styled from 'styled-components';
 
 import { colors } from '~/styles/colors';
 
-export const Container = styled.div``;
-export const ActionContainer = styled.div``;
-export const ModalContainer = styled.div``;
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr 0.5fr;
+  height: 57px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  padding-left: 25px;
+  padding-right: 13px;
+  background: #fff;
+  border-radius: 4px;
+
+  img {
+    width: 35px;
+    height: 35px;
+    align-self: center;
+    border-radius: 50%;
+  }
+
+  > small {
+    font-size: 16px;
+    color: ${colors.label};
+    text-align: left;
+    margin: auto 0;
+  }
+
+  > small:last-child {
+    text-align: right;
+  }
+
+  > section {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+`;
+export const ActionContainer = styled.div`
+  padding: 10px;
+
+  > div {
+    display: flex;
+    align-items: center;
+
+    button {
+      background: none;
+      border: none;
+      display: flex;
+    }
+
+    svg {
+      margin-right: 8px;
+    }
+
+    span {
+      font-size: 14px;
+      color: ${colors.input};
+    }
+  }
+
+  > div:first-child {
+    padding-bottom: 9px;
+    border-bottom: 1px solid ${colors.border};
+  }
+
+  div:nth-last-child(1) {
+    padding-top: 9px;
+  }
+`;
+export const ModalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 25px;
+
+  strong {
+    color: ${colors.input};
+    font-size: 14px;
+    margin-bottom: 5px;
+  }
+
+  p {
+    font-size: 16px;
+    color: ${colors.label};
+    line-height: 26px;
+  }
+`;
