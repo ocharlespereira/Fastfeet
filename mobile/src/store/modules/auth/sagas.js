f06b9a290658188e720b26fe94519df4416799dd
Charles Pereira <charlesti.ptu@gmail.com>
2020-03-18 00:07:53 -0300

    store: configurando conexão segura, action, reducer e saga

---
+++ mobile/src/store/modules/auth/sagas.js
@@ -0,0 +1,29 @@
+import { Alert } from 'react-native'
+
+import { parseISO, format } from 'date-fns'
+import { takeLatest, call, all, put } from 'redux-saga/effects';
+
+import api from '~/services/api';
+
+import { signInSuccess, signFailure } from './actions';
+
+export function* signIn({ payload }) {
+  try {
+    const { id } = payload;
+
+    const response = yield call(api.get, `orders/${id}`);
+
+    yield put(signInSuccess(id, {
+      name: response.data.name,
+      email: response.data.email,
+      created_at: format(parseISO(response.data.created_at), 'dd/MM/yyyy'),
+      avatar: response.data.avatar,
+    }));
+
+  } catch (error) {
+    Alert.alert('Falha na autenticação, verifique seus dados.');
+    yield put(signFailure());
+  }
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
